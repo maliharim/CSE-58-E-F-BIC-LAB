8d060151fc5ed880af8e7caa5ecd32c09fcb536e
maliharim <malihareem300@gmail.com>
2026-06-09 15:55:38 +0600

    Update Problem3.py

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,15 @@
-
+k=input()
+rev_k=k[::-1]
+rev_c=""
+for x in rev_k:
+    if x== "A":
+        rev_c +="T"
+    elif x=="T":
+        rev_c +="A"
+    elif x== "C":
+        rev_c +="G"
+    elif x=="G":
+        rev_c +="C"
+        
+  
+print(rev_c)
