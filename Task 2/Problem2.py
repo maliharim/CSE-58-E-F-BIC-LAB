c6f72f6e1738d75c20027b09dae8441691ddf623
maliharim <malihareem300@gmail.com>
2026-06-09 15:54:57 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,15 @@
+exam = input()
+m = int(input())    
 
+count={}
+for i in range(len(exam)- m+1):
+    s = exam[i:i+m]
+    if s in count:
+        count[s] += 1
+    else:
+        count[s] = 1
+max_count = max(count.values())
+
+for s in count:
+    if count[s]==max_count:
+       print(s)
