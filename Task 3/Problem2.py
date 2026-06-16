47d9160677be63742dd1a3b42935e6d0032151a2
maliharim <malihareem300@gmail.com>
2026-06-16 16:19:25 +0600

    Add pattern matching with allowed mismatches
    Implement pattern matching with mismatches

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,16 @@
+pattern = input()
+text = input()
 
+d = int(input())
+
+length = len(pattern)
+
+for i in range(len(text)-length+1):
+    mismatch = 0
+
+    for j in range(length):
+        if(pattern[j] != text[i+j]):
+            mismatch += 1
+
+    if mismatch <=d:
+        print(i,end='  ')
